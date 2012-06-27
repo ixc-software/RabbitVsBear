2d5698c9be71ecd16a58b10349c47c5dd9d5c7b3
Oleksii Vynogradov <alex@MacBook-Pro-Oleksii.local>
2012-06-27 19:47:37 +0300

    1.0.1

--- RabbitVsBear/DataViewController.h
+++ RabbitVsBear/DataViewController.h
@@ -22,24 +22,24 @@
 @property (strong) NSNumber *currentSelectedFlagTag;
 @property (strong) NSTimer *progressTimer;
 
-@property (weak, nonatomic) IBOutlet UIImageView *image;
-@property (weak, nonatomic) IBOutlet UIImageView *imageBackground;
-@property (weak, nonatomic) IBOutlet UIButton *previousPage;
-@property (weak, nonatomic) IBOutlet UIButton *nextPage;
-@property (weak, nonatomic) IBOutlet UIButton *flagAE;
-@property (weak, nonatomic) IBOutlet UIButton *flagDE;
-@property (weak, nonatomic) IBOutlet UIButton *flagCN;
-@property (weak, nonatomic) IBOutlet UIButton *flagSP;
-@property (weak, nonatomic) IBOutlet UIButton *flagRUS;
-@property (weak, nonatomic) IBOutlet UIButton *flagGB;
-@property (weak, nonatomic) IBOutlet UIButton *flagUA;
-@property (weak, nonatomic) IBOutlet UIButton *playStop;
+@property (nonatomic) IBOutlet UIImageView *image;
+@property (nonatomic) IBOutlet UIImageView *imageBackground;
+@property (nonatomic) IBOutlet UIButton *previousPage;
+@property (nonatomic) IBOutlet UIButton *nextPage;
+@property (nonatomic) IBOutlet UIButton *flagAE;
+@property (nonatomic) IBOutlet UIButton *flagDE;
+@property (nonatomic) IBOutlet UIButton *flagCN;
+@property (nonatomic) IBOutlet UIButton *flagSP;
+@property (nonatomic) IBOutlet UIButton *flagRUS;
+@property (nonatomic) IBOutlet UIButton *flagGB;
+@property (nonatomic) IBOutlet UIButton *flagUA;
+@property (nonatomic) IBOutlet UIButton *playStop;
 
-@property (weak, nonatomic) IBOutlet UIView *buttonsView;
+@property (nonatomic) IBOutlet UIView *buttonsView;
 
-@property (weak, nonatomic) IBOutlet UITextView *text;
-@property (weak, nonatomic) IBOutlet UITextView *finalTextInsideBox;
-@property (weak, nonatomic) IBOutlet UIProgressView *playbackProgress;
-@property (weak, nonatomic) IBOutlet UILabel *pageNumber;
+@property (nonatomic) IBOutlet UITextView *text;
+@property (nonatomic) IBOutlet UITextView *finalTextInsideBox;
+@property (nonatomic) IBOutlet UIProgressView *playbackProgress;
+@property (nonatomic) IBOutlet UILabel *pageNumber;
 
 @end
